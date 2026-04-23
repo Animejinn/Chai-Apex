d4cd773e8e74132cb7841ea34ae1579901065a1b
AnimeJinn <animejinn@gmail.com>
2026-04-23 23:48:24 +0500

    ./WaterWatermelon(CF-ProbSet)(Kumail).cpp

--- Watermelon(CF-ProbSet)(Kumail).cpp
+++ Watermelon(CF-ProbSet)(Kumail).cpp
@@ -1,28 +1,15 @@
 #include<iostream>
-
 using namespace std;
 
 int main()
 {
-    int w = -1;
-    while(!(1 <= w && w <= 100))
-    {
-        cout<<"Give the weight of the Watermelon: ";
-        cin>>w;
-    }
-    
-    
-    if(w % 2 == 0)
-    {
-        if((w/2) % 2 == 0)
-        {
-            cout<<"YES";
-        }
-    }
-    else
-    {
-        cout<<"NO";
-    }
-    return 0;
+    int w;
+    cin >> w;
 
+    if(w > 2 && w % 2 == 0)
+        cout << "YES";
+    else
+        cout << "NO";
+
+    return 0;
 }
