55c4ace528217f598e6bbbe972c86a34bc68f83f
AnimeJinn <animejinn@gmail.com>
2026-04-24 14:09:19 +0500

    Bit++(CF-ProbSet)(Kumail).cpp

--- Bit++(CF-ProbSet)(Kumail).cpp
+++ Bit++(CF-ProbSet)(Kumail).cpp
@@ -5,20 +5,21 @@ using namespace std;
 int main()
 {
     int n;
-    string opX[100];
+    string opX[150];
     int xVal = 0;
     cin>>n;
     for(int i = 0; i < n; i++)
     {
         cin>>opX[i];
-        if(opX[i] == "++x" || opX[i] == "x++")
+        if(opX[i] == "++X" || opX[i] == "X++")
         {
             xVal++;
         }
-        else if(opX[i] == "--x" || opX[i] == "x--")
+        else if(opX[i] == "--X" || opX[i] == "X--")
         {
             xVal--;
         }
     }
     cout<<xVal;
+    return 0;
 }
