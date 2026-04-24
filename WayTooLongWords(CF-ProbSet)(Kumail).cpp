204bec8c53f724fb2e5e37920bdc917c8d390341
AnimeJinn <animejinn@gmail.com>
2026-04-24 09:04:09 +0500

    WayTooLongWords(CF-ProbSet)(Kumail).cpp

---
+++ WayTooLongWords(CF-ProbSet)(Kumail).cpp
@@ -0,0 +1,31 @@
+//77A. Way Too Long Words
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int size;
+    string word[100];
+    cin>>n;
+    for(int i = 0; i < n; i++)
+    {
+        cin>>word[i];
+    }
+    for(int i = 0; i < n; i++)
+    {
+        size = word[i].length();
+        if(size > 10)
+        {
+            cout<<word[i][0]<<size-2<<word[i][size-1]<<endl;
+        }
+        else
+        {
+            cout<<word;
+        }
+    }
+    return 0;
+}
