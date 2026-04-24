823b1abd564575e426ea2821abb85a66653a8175
AnimeJinn <animejinn@gmail.com>
2026-04-24 14:05:23 +0500

    Team(CF-ProbSet)(Kumail).cpp

--- Team(CF-ProbSet)(Kumail).cpp
+++ Team(CF-ProbSet)(Kumail).cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main()
 {
-    int p;
-    int choice[3];
-    int solP = 0;
+    short p;
+    short choice[3];
+    short solP = 0;
     cin>>p;
-    for(int i = 0; i < p; i++)
+    for(short i = 0; i < p; i++)
     {
         cin>>choice[1]>>choice[2]>>choice[3];
         if(choice[1]+choice[2]+choice[3] > 1)
