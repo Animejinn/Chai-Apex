033de05573621f91bb05486c19b45727654ee9bd
AnimeJinn <animejinn@gmail.com>
2026-04-26 14:35:28 +0500

    BoyorGirl(CF-ProbSet)(Kumail).cpp

---
+++ BoyorGirl(CF-ProbSet)(Kumail).cpp
@@ -0,0 +1,49 @@
+// 236A. Boy or Girl 
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int marked[27];
+    char usrName[101];
+    int unChar = 0;
+    cin>>usrName;
+
+    for(int i = 0; i <= strlen(usrName); i++)
+    {
+        if(int('a') <= int(usrName[i]) && int(usrName[i]) <= int('z'))
+        {
+            for(int k = 0; k <= 27 && i >= 1; k++)
+            {
+                if(usrName[i] == char(marked[k]))
+                {
+                    unChar--;
+                }
+            }
+            for(int j = 97; j <= int('z'); j++)
+            {
+                if(usrName[i] == char(j))
+                {
+                    for(int l = 0; 0 < i-l && l < 27; l++)
+                    {
+                        if(marked[i-l] != char(j))
+                        {
+                            marked[i] = j;
+                        }
+                    }
+                    unChar++;
+                }
+            }
+        }
+    }
+    if(unChar % 2 == 0)
+    {
+        cout<<"CHAT WITH HER!";
+    }
+    else
+    {
+        cout<<"IGNORE HIM!";
+    }
+}
