34b8542cdac3709737d8344f09c3b14a05790287
Animejinn <ahmadkumail432@gmail.com>
2026-05-01 23:32:59 +0500

    Yea

---
+++ way too long words cpp.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    int size;
+    string word[100];
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> word[i];
+    }
+    for (int i = 0; i < n; i++) {
+        size = word[i].length();
+
+        if (size > 10) {
+            cout << word[i][0] << size - 2 << word[i][size - 1] << endl;
+        }
+        else {
+            cout << word[i] << endl;
+        }
+    }
+    return 0;
+}
